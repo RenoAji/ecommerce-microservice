eca980d0369cd4530f58993cea36ffffd935a5e8
Septareno Nugroho Aji <renoaji25sep@gmail.com>
2026-03-07 20:57:39 +0700

    feat: add /health to delivery service (also testing CI/CD pipeline)

--- delivery-service/cmd/api/main.go
+++ delivery-service/cmd/api/main.go
@@ -124,6 +124,9 @@ func main() {
 			delivery.GET("/:id", hdl.GetDelivery)
 			delivery.PUT("/:id/status", hdl.UpdateDeliveryStatus)
 		}
+		delivery.GET("/health", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{"status": "ok"})
+		})
 	}
 
 	// Start gRPC Server in a goroutine
