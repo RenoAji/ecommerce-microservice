c1c5c2923535d0572ca5d52d514be5d711875f4f
Septareno Nugroho Aji <renoaji25sep@gmail.com>
2026-03-10 00:47:03 +0700

    fix: test(2)

--- order-service/cmd/api/main.go
+++ order-service/cmd/api/main.go
@@ -139,7 +139,7 @@ func main() {
 			order.GET("", hdl.GetOrders)
 			order.GET("/:id", hdl.GetOrderByID)
 			order.GET("/health", func(c *gin.Context) {
-				c.JSON(http.StatusOK, gin.H{"status": "ok"})
+				c.JSON(http.StatusOK, gin.H{"status": "okk"})
 			})
 		}
 	}
